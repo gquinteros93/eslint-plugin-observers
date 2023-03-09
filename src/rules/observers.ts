23d45e4d01fac23c70340770c054f8b723c8c478
German Quinteros <german.quinteros.1993@gmail.com>
2023-03-09 21:30:55 +0100

    [no-ticket] add eslint rules

--- src/rules/observers.ts
+++ src/rules/observers.ts
@@ -1,4 +1,4 @@
-import { Rule } from "eslint";
+import { Rule } from 'eslint'
 import {
   BaseCallExpression,
   Expression,
@@ -8,7 +8,7 @@ import {
   VariableDeclarator,
   NewExpression,
   AssignmentExpression,
-} from "estree";
+} from 'estree'
 import {
   isNodeMemberExpression,
   isNodeIdentifier,
@@ -16,19 +16,19 @@ import {
   RuleType,
   getDescription,
   isNewExpression,
-} from "../utils";
-import merge = require("lodash/merge");
+} from '../utils'
+import merge = require('lodash/merge');
 
 enum ObserversTypes {
-  MutationObserver = "MutationObserver",
-  ResizeObserver = "ResizeObserver",
-  IntersectionObserver = "IntersectionObserver",
+  MutationObserver = 'MutationObserver',
+  ResizeObserver = 'ResizeObserver',
+  IntersectionObserver = 'IntersectionObserver',
 }
 
 enum MethodsType {
-  OBSERVE = "observe",
-  UNOBSERVE = "unobserve",
-  DISCONNECT = "disconnect",
+  OBSERVE = 'observe',
+  UNOBSERVE = 'unobserve',
+  DISCONNECT = 'disconnect',
 }
 
 interface ObservedElements {
@@ -70,8 +70,8 @@ const reportMissingUnobserveOrDisconnect = ({
   context.report({
     loc,
     message: `${element} does not have a corresponding unobserve or disconnnect`,
-  });
-};
+  })
+}
 
 const reportNoMatchingUnobserveTarget = ({
   context,
@@ -87,158 +87,158 @@ const reportNoMatchingUnobserveTarget = ({
   context.report({
     loc,
     message: `there isn't an unobserve invoke for ${element} with target ${targetObserve}`,
-  });
-};
+  })
+}
 
 const isNewObserver = (node: Expression): boolean => {
-  const newExpression: NewExpression = <NewExpression>node;
-  const identifier: Identifier = <Identifier>newExpression.callee;
-  const observerType = identifier?.name as ObserversTypes;
+  const newExpression: NewExpression = <NewExpression>node
+  const identifier: Identifier = <Identifier>newExpression.callee
+  const observerType = identifier?.name as ObserversTypes
   return [ObserversTypes.MutationObserver, ObserversTypes.IntersectionObserver, ObserversTypes.ResizeObserver].includes(
     observerType
-  );
-};
+  )
+}
 
 const assignmentExpressionListener =
   (newObservers: NewObservers, assignedObservers: AssignedObservers) => (node: AssignmentExpression) => {
     if (isNewExpression(node.right) && isNewObserver(node.right)) {
-      const left: MemberExpression = <MemberExpression>node.left;
-      const name = (<Identifier>left.property)?.name;
+      const left: MemberExpression = <MemberExpression>node.left
+      const name = (<Identifier>left.property)?.name
       if (name) {
         newObservers[name] = {
           loc: node.loc,
-        };
+        }
       }
     } else if (isNodeIdentifier(node.right)) {
-      const name = (<Identifier>node.right)?.name;
+      const name = (<Identifier>node.right)?.name
       if (name && newObservers[name]) {
-        const left: MemberExpression = <MemberExpression>node.left;
-        const nameleft = parseMemberExpression(left);
+        const left: MemberExpression = <MemberExpression>node.left
+        const nameleft = parseMemberExpression(left)
         assignedObservers[name] = {
           assigned: nameleft,
           loc: node.loc,
-        };
+        }
       }
     }
-  };
+  }
 
 const variableDeclaratorListener =
   (newObservers: NewObservers, assignedObservers: AssignedObservers) => (node: VariableDeclarator) => {
     if (node?.init && isNewExpression(node?.init) && isNewObserver(node?.init)) {
-      const variable: Identifier = <Identifier>node.id;
+      const variable: Identifier = <Identifier>node.id
       newObservers[variable.name] = {
         loc: variable.loc,
-      };
+      }
     } else if (node?.init && isNodeIdentifier(node?.init)) {
-      const name = (<Identifier>node?.init)?.name;
+      const name = (<Identifier>node?.init)?.name
       if (name && newObservers[name]) {
-        const variable: Identifier = <Identifier>node.id;
+        const variable: Identifier = <Identifier>node.id
         assignedObservers[name] = {
           assigned: variable.name,
           loc: node.loc,
-        };
+        }
       }
     }
-  };
+  }
 
 const callExpressionListener = (methods: Methods) => (node: BaseCallExpression) => {
   if (isNodeMemberExpression(node.callee)) {
-    const callee: MemberExpression = <MemberExpression>node.callee;
-    const methodsType = (<Identifier>callee.property)?.name as MethodsType;
+    const callee: MemberExpression = <MemberExpression>node.callee
+    const methodsType = (<Identifier>callee.property)?.name as MethodsType
 
     if ([MethodsType.OBSERVE, MethodsType.UNOBSERVE].includes(methodsType) && node.arguments.length) {
-      const element = parseMemberExpression(callee);
-      const argument = <Expression>node.arguments[0];
-      let target: string;
+      const element = parseMemberExpression(callee)
+      const argument = <Expression>node.arguments[0]
+      let target: string
 
       if (isNodeIdentifier(argument)) {
-        target = (<Identifier>argument).name;
+        target = (<Identifier>argument).name
       } else {
-        target = parseMemberExpression(<MemberExpression>argument);
+        target = parseMemberExpression(<MemberExpression>argument)
       }
 
-      const currentMethods = methods[<MethodsType>methodsType] || {};
+      const currentMethods = methods[<MethodsType>methodsType] || {}
       methods[<MethodsType>methodsType] = merge(currentMethods, {
         [element]: {
           [target]: {
             loc: node.loc,
           },
         },
-      });
+      })
     } else if ([MethodsType.DISCONNECT].includes(methodsType)) {
-      const element = parseMemberExpression(callee);
+      const element = parseMemberExpression(callee)
 
-      const currentMethods = methods[<MethodsType>methodsType] || {};
+      const currentMethods = methods[<MethodsType>methodsType] || {}
       methods[<MethodsType>methodsType] = merge(currentMethods, {
         [element]: {
           [MethodsType.DISCONNECT]: {
             loc: node.loc,
           },
         },
-      });
+      })
     }
   }
-};
+}
 
 const programListener =
   (ruleName: RuleType, methods: Methods, assignedObservers: AssignedObservers, context: Rule.RuleContext) => () => {
-    const observeMethods = methods[MethodsType.OBSERVE] ?? {};
-    const unobserveMethods = methods[MethodsType.UNOBSERVE] ?? {};
-    const disconnectMethods = methods[MethodsType.DISCONNECT] ?? {};
+    const observeMethods = methods[MethodsType.OBSERVE] ?? {}
+    const unobserveMethods = methods[MethodsType.UNOBSERVE] ?? {}
+    const disconnectMethods = methods[MethodsType.DISCONNECT] ?? {}
 
     Object.keys(observeMethods).forEach((element) => {
-      const observed = observeMethods[element];
-      const unobserved = unobserveMethods[element];
-      const disconnected = disconnectMethods[element];
-      const assigned = assignedObservers[element];
-      const assignedUnobserved = assigned ? unobserveMethods[assigned.assigned] : null;
-      const assignedDisconnected = assigned ? disconnectMethods[assigned.assigned] : null;
+      const observed = observeMethods[element]
+      const unobserved = unobserveMethods[element]
+      const disconnected = disconnectMethods[element]
+      const assigned = assignedObservers[element]
+      const assignedUnobserved = assigned ? unobserveMethods[assigned.assigned] : null
+      const assignedDisconnected = assigned ? disconnectMethods[assigned.assigned] : null
 
       Object.entries(observed).forEach(([target, { loc }]) => {
         switch (ruleName) {
-          case RuleType.NoMissingUnobserveOrDisconnect:
-            if (!unobserved && !disconnected && !assignedUnobserved && !assignedDisconnected) {
-              const reportedElement = assigned?.assigned ?? element;
-              reportMissingUnobserveOrDisconnect({ context, element: reportedElement, loc });
-            }
-            break;
+        case RuleType.NoMissingUnobserveOrDisconnect:
+          if (!unobserved && !disconnected && !assignedUnobserved && !assignedDisconnected) {
+            const reportedElement = assigned?.assigned ?? element
+            reportMissingUnobserveOrDisconnect({ context, element: reportedElement, loc })
+          }
+          break
 
-          case RuleType.MatchingUnobserveTarget:
-            if (unobserved && !unobserved[target]) {
-              reportNoMatchingUnobserveTarget({ context, element, targetObserve: target, loc });
-            } else if (assignedUnobserved && !assignedUnobserved[target]) {
-              const reportedElement = assigned?.assigned ?? element;
-              reportNoMatchingUnobserveTarget({ context, element: reportedElement, targetObserve: target, loc });
-            }
-            break;
+        case RuleType.MatchingUnobserveTarget:
+          if (unobserved && !unobserved[target]) {
+            reportNoMatchingUnobserveTarget({ context, element, targetObserve: target, loc })
+          } else if (assignedUnobserved && !assignedUnobserved[target]) {
+            const reportedElement = assigned?.assigned ?? element
+            reportNoMatchingUnobserveTarget({ context, element: reportedElement, targetObserve: target, loc })
+          }
+          break
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
 export const createRule = (ruleName: RuleType): Rule.RuleModule => ({
   meta: {
     docs: {
       description: getDescription(ruleName),
-      category: "Best Practices",
+      category: 'Best Practices',
       recommended: true,
-      url: "",
+      url: '',
     },
     schema: [],
   },
   create: (context: Rule.RuleContext): Rule.RuleListener => {
-    const methods: Methods = {};
-    const newObservers: NewObservers = {};
-    const assignedObservers: AssignedObservers = {};
+    const methods: Methods = {}
+    const newObservers: NewObservers = {}
+    const assignedObservers: AssignedObservers = {}
     return {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      "VariableDeclarator:exit": variableDeclaratorListener(newObservers, assignedObservers),
-      "AssignmentExpression:exit": assignmentExpressionListener(newObservers, assignedObservers),
+      'VariableDeclarator:exit': variableDeclaratorListener(newObservers, assignedObservers),
+      'AssignmentExpression:exit': assignmentExpressionListener(newObservers, assignedObservers),
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      "CallExpression:exit": callExpressionListener(methods),
-      "Program:exit": programListener(ruleName, methods, assignedObservers, context),
-    };
+      'CallExpression:exit': callExpressionListener(methods),
+      'Program:exit': programListener(ruleName, methods, assignedObservers, context),
+    }
   },
-});
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,41 +1,41 @@
-import { Expression, MemberExpression, Super, Identifier } from "estree";
+import { Expression, MemberExpression, Super, Identifier } from 'estree'
 
 export enum RuleType {
-  NoMissingUnobserveOrDisconnect = "no-missing-unobserve-or-disconnect",
-  MatchingUnobserveTarget = "matching-unobserve-target",
+  NoMissingUnobserveOrDisconnect = 'no-missing-unobserve-or-disconnect',
+  MatchingUnobserveTarget = 'matching-unobserve-target',
 }
 
-export const isNewExpression = (node: Expression | Super): boolean => node?.type === "NewExpression";
-export const isNodeIdentifier = (node: Expression | Super): boolean => node?.type === "Identifier";
-export const isNodeMemberExpression = (node: Expression | Super): boolean => node?.type === "MemberExpression";
-export const isNodeThisExpression = (node: Expression | Super): boolean => node?.type === "ThisExpression";
-export const isNodeFunctionExpression = (node: Expression | Super): boolean => node?.type === "FunctionExpression";
+export const isNewExpression = (node: Expression | Super): boolean => node?.type === 'NewExpression'
+export const isNodeIdentifier = (node: Expression | Super): boolean => node?.type === 'Identifier'
+export const isNodeMemberExpression = (node: Expression | Super): boolean => node?.type === 'MemberExpression'
+export const isNodeThisExpression = (node: Expression | Super): boolean => node?.type === 'ThisExpression'
+export const isNodeFunctionExpression = (node: Expression | Super): boolean => node?.type === 'FunctionExpression'
 export const isNodeArrowFunctionExpression = (node: Expression | Super): boolean =>
-  node?.type === "ArrowFunctionExpression";
+  node?.type === 'ArrowFunctionExpression'
 
 export const parseMemberExpression = (node: MemberExpression): string => {
-  let value;
+  let value
 
   if (isNodeIdentifier(node.object)) {
-    value = (<Identifier>node.object).name;
+    value = (<Identifier>node.object).name
   }
 
   if (isNodeMemberExpression(node.object)) {
-    value = parseMemberExpression(<MemberExpression>node.object);
+    value = parseMemberExpression(<MemberExpression>node.object)
   }
 
   if (isNodeThisExpression(node.object)) {
-    value = `this.${(<Identifier>node.property).name}`;
+    value = `this.${(<Identifier>node.property).name}`
   }
 
-  return value;
-};
+  return value
+}
 
 export const getDescription = (ruleName: RuleType): string => {
   switch (ruleName) {
-    case RuleType.NoMissingUnobserveOrDisconnect:
-      return "Missing disconnect or unobserve if observe exists";
-    case RuleType.MatchingUnobserveTarget:
-      return "Target for unobserve should match target observed previously";
+  case RuleType.NoMissingUnobserveOrDisconnect:
+    return 'Missing disconnect or unobserve if observe exists'
+  case RuleType.MatchingUnobserveTarget:
+    return 'Target for unobserve should match target observed previously'
   }
-};
+}
